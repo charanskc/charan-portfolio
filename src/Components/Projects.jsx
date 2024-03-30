6abcc2a871c6cb1ca0411fb58cc8db85051aa64d
Sai Charan <charanskc7@gmail.com>
2024-03-31 04:22:35 +0530

    new updated have been added to the portfolio website

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Projects.css";
+import amazonImage from "../assets/amazon shopping.jpg"
+import Game2048 from "../assets/2048gamesnapshot.jpg"
+import lostandfound from "../assets/lafimgae.png"
 
 function Projects() {
   return (
@@ -11,17 +14,17 @@ function Projects() {
 
         <div className="projects__mainbody">
             <div className="project__one">
-                <img src="" alt="amazon_svg" />
+                <img src={amazonImage} alt="amazon_svg" className='projectsAmazonImage' />
                 <h2>Amazon-Clone</h2>
                 <p>A copy of amazon with more advanced features using REACT-JS.</p>
             </div>
             <div className="project__two">
-                <img src="" alt="2048_svg" />
+                <img src={Game2048} alt="2048_svg" className='Game2048Image' />
                 <h2>2048-Game</h2>
                 <p>A game of 2048 as web application.</p>
             </div>
             <div className="project__three">
-                <img src="" alt="laf_svg" />
+                <img src={lostandfound} alt="laf_svg" className='lostandfoundImage'/>
                 <h2>Lost & Found</h2>
                 <p>A platform where people from an organization can go if they find any items or if they loose.</p>
             </div>
